b239a14a368db3f8f70441609ddae8df35792fc6
raul <roaslin@gmail.com>
2020-12-31 11:17:34 +0100

    refactor to fun compment

--- redux/songs/src/components/SongList.js
+++ redux/songs/src/components/SongList.js
@@ -1,22 +1,22 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { selectSong } from "../actions";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { selectSong } from '../actions';
 
-class SongList extends Component {
-  listOfSongs = () => {
-    return this.props.songs.map((song) => {
+const SongList = ({songs}) => {
+  const listOfSongs = () => {
+    return songs.map((song) => {
       return (
-        <div key={song.title} className="item">
-          <div className="right floated content">
+        <div key={song.title} className='item'>
+          <div className='right floated content'>
             <button
-              className="ui button"
-              onClick={() => this.props.selectSong(song)}
+              className='ui button'
+              onClick={() => selectSong(song)}
             >
               Select
             </button>
           </div>
-          <div className="content">
-            <div className="header">Title: {song.title}</div>
+          <div className='content'>
+            <div className='header'>Title: {song.title}</div>
             Length: {song.length}
           </div>
         </div>
@@ -24,14 +24,12 @@ class SongList extends Component {
     });
   };
 
-  render() {
-    return (
-      <div className="ui segment">
-        <div className="ui relaxed divided list">{this.listOfSongs()}</div>
-      </div>
-    );
-  }
-}
+  return (
+    <div className='ui segment'>
+      <div className='ui relaxed divided list'>{listOfSongs()}</div>
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => {
   return { songs: state.songs };
