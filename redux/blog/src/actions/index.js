05e0b8978a496c589206d7cb8c6d6e030d656a7f
raul <roaslin@gmail.com>
2020-12-31 10:13:05 +0100

    install lodash

--- redux/blog/src/actions/index.js
+++ redux/blog/src/actions/index.js
@@ -1,15 +1,10 @@
+import _ from 
 import jsonplaceholder from '../apis/jsonPlaceholder';
 
 export const fetchPosts = () => async (dispatch) => {
   const response = await jsonplaceholder.get('/posts');
 
   dispatch({ type: 'FETCH_POSTS', payload: response.data });
-};
-
-export const fetchUsers = () => async (dispatch) => {
-  const response = await jsonplaceholder.get(`/users`);
-
-  dispatch({ type: 'FETCH_USERS', payload: response.data });
 };
 
 export const fetchUser = (id) => async (dispatch) => {
