bb55e8a498a9c8caf16b0eb2834ef3b6ee67ce4e
raul <roaslin@gmail.com>
2021-01-03 11:44:04 +0100

    store userId

--- redux/streams/client/src/reducers/authReducer.js
+++ redux/streams/client/src/reducers/authReducer.js
@@ -1,13 +1,13 @@
 import { SIGN_IN, SIGN_OUT } from '../actions/types';
 
-const INITIAL_STATE = { isSignedIn: null };
+const INITIAL_STATE = { isSignedIn: null, userId: null };
 
 export default (state = INITIAL_STATE, action) => {
   if (action.type === SIGN_IN) {
-    return { ...state, isSignedIn: true };
+    return { ...state, isSignedIn: true, userId: action.payload };
   }
   if (action.type === SIGN_OUT) {
-    return { ...state, isSignedIn: false };
+    return { ...state, isSignedIn: false, userId: null };
   }
 
   return state;
