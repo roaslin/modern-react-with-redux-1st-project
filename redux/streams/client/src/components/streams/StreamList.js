0218c77331c8f5428b8d8dcc5ca2c9714a181081
raul <roaslin@gmail.com>
2021-01-10 17:08:04 +0100

    show admin buttons when userId corresponds to streams

--- redux/streams/client/src/components/streams/StreamList.js
+++ redux/streams/client/src/components/streams/StreamList.js
@@ -1,16 +1,41 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchStreams } from '../../actions';
 
-const StreamList = ({ streams, fetchStreams }) => {
+const StreamList = ({ streams, currentUserId, isSignedIn, fetchStreams }) => {
   useEffect(() => {
     fetchStreams();
-  }, []);
+  }, [fetchStreams]);
+
+  const renderAdmin = (stream) => {
+    if (stream.userId === currentUserId) {
+      return (
+        <div className='right floated content'>
+          <button className='ui button primary'>Edit</button>
+          <button className='ui button negative'>Delete</button>
+        </div>
+      );
+    }
+  };
+
+  const renderCreateStream = () => {
+    if (isSignedIn) {
+      return (
+        <div style={{ textAlign: 'right' }}>
+          <Link to='/streams/new' className='ui button primary'>
+            Create Stream
+          </Link>
+        </div>
+      );
+    }
+  };
 
   const renderList = () => {
     return streams.map((stream) => {
       return (
         <div className='item' key={stream.id}>
+          {renderAdmin(stream)}
           <i className='large middle aligned icon camera' />
           <div className='content'>
             {stream.title}
@@ -25,12 +50,17 @@ const StreamList = ({ streams, fetchStreams }) => {
     <div>
       <h2>Streams</h2>
       <div className='ui celled list'>{renderList()}</div>
+      {renderCreateStream()}
     </div>
   );
 };
 
 const mapStateToProps = (state) => {
-  return { streams: Object.values(state.streams) };
+  return {
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId,
+    isSignedIn: state.auth.isSignedIn,
+  };
 };
 
 export default connect(mapStateToProps, { fetchStreams })(StreamList);
